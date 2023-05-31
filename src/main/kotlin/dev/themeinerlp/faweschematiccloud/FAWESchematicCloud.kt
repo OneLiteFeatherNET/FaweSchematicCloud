a95c6bfd66a02fc3c61f42cb7352086dfd0433a1
TheMeinerLP <p.glanz@madfix.me>
2023-05-31 16:32:31 +0200

    Add main clas

---
+++ src/main/kotlin/dev/themeinerlp/faweschematiccloud/FAWESchematicCloud.kt
@@ -0,0 +1,11 @@
+package dev.themeinerlp.faweschematiccloud
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class FAWESchematicCloud : JavaPlugin() {
+
+    override fun onEnable() {
+        super.onEnable()
+    }
+
+}
