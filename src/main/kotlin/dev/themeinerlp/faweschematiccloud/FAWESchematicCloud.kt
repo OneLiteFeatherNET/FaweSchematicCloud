2fbc1c8199bdd4f0e178bfeae5bac2ee803a6bad
TheMeinerLP <p.glanz@madfix.me>
2023-05-31 17:06:34 +0200

    Add schematic upload to main

--- src/main/kotlin/dev/themeinerlp/faweschematiccloud/FAWESchematicCloud.kt
+++ src/main/kotlin/dev/themeinerlp/faweschematiccloud/FAWESchematicCloud.kt
@@ -1,8 +1,13 @@
 package dev.themeinerlp.faweschematiccloud
 
+import dev.themeinerlp.faweschematiccloud.util.SchematicUploader
 import org.bukkit.plugin.java.JavaPlugin
 
 class FAWESchematicCloud : JavaPlugin() {
+
+    val schematicUploader: SchematicUploader by lazy {
+        SchematicUploader(this)
+    }
 
     override fun onEnable() {
         super.onEnable()
