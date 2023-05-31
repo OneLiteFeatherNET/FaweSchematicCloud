938d24bf2425c3c104f2420c76ec6e90b4ac1b10
TheMeinerLP <p.glanz@madfix.me>
2023-05-31 17:54:42 +0200

    Extend result

--- src/main/kotlin/dev/themeinerlp/faweschematiccloud/util/SchematicUploadResult.kt
+++ src/main/kotlin/dev/themeinerlp/faweschematiccloud/util/SchematicUploadResult.kt
@@ -2,6 +2,8 @@ package dev.themeinerlp.faweschematiccloud.util
 
 data class SchematicUploadResult(
     val success: Boolean,
+    val apiDownloadUrl: String? = null,
+    val apiDeletionUrl: String? = null,
     val downloadUrl: String? = null,
     val deletionUrl: String? = null,
 )
