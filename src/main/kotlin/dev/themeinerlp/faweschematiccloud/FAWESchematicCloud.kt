d64ced01fdf6688fe71a5508bf8c35d280c922c1
TheMeinerLP <p.glanz@madfix.me>
2023-05-31 17:55:33 +0200

    Register command

--- src/main/kotlin/dev/themeinerlp/faweschematiccloud/FAWESchematicCloud.kt
+++ src/main/kotlin/dev/themeinerlp/faweschematiccloud/FAWESchematicCloud.kt
@@ -1,7 +1,17 @@
 package dev.themeinerlp.faweschematiccloud
 
+import cloud.commandframework.annotations.AnnotationParser
+import cloud.commandframework.arguments.parser.ParserParameters
+import cloud.commandframework.arguments.parser.StandardParameters
+import cloud.commandframework.bukkit.CloudBukkitCapabilities
+import cloud.commandframework.execution.CommandExecutionCoordinator
+import cloud.commandframework.meta.CommandMeta
+import cloud.commandframework.paper.PaperCommandManager
+import dev.themeinerlp.faweschematiccloud.commands.DownloadCommand
 import dev.themeinerlp.faweschematiccloud.util.SchematicUploader
+import org.bukkit.command.CommandSender
 import org.bukkit.plugin.java.JavaPlugin
+import java.util.function.Function
 
 class FAWESchematicCloud : JavaPlugin() {
 
@@ -9,8 +19,40 @@ class FAWESchematicCloud : JavaPlugin() {
         SchematicUploader(this)
     }
 
+    val paperCommandManager: PaperCommandManager<CommandSender> by lazy {
+        PaperCommandManager(
+            this,
+            CommandExecutionCoordinator.simpleCoordinator(),
+            Function.identity(),
+            Function.identity()
+        )
+    }
+
+    val annotationParser: AnnotationParser<CommandSender> by lazy {
+        if (paperCommandManager.hasCapability(CloudBukkitCapabilities.BRIGADIER)) {
+            paperCommandManager.registerBrigadier()
+            this.getLogger().info("Brigadier support enabled")
+        }
+        if (paperCommandManager.hasCapability(CloudBukkitCapabilities.ASYNCHRONOUS_COMPLETION)) {
+            paperCommandManager.registerAsynchronousCompletions()
+            this.getLogger().info("Asynchronous completions enabled")
+        }
+        val commandMetaFunction =
+            Function<ParserParameters, CommandMeta> { p: ParserParameters ->
+                CommandMeta.simple().with(
+                    CommandMeta.DESCRIPTION,
+                    p.get(StandardParameters.DESCRIPTION, "No description")
+                ).build()
+            }
+        AnnotationParser(
+            paperCommandManager,
+            CommandSender::class.java, commandMetaFunction
+        )
+    }
+
     override fun onEnable() {
-        super.onEnable()
+        saveDefaultConfig()
+        annotationParser.parse(DownloadCommand(this))
     }
 
 }
