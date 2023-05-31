b1b370faa3d1fd265b4780b303547f96d149429c
TheMeinerLP <p.glanz@madfix.me>
2023-05-31 17:55:23 +0200

    Add download command to override it from fawe

---
+++ src/main/kotlin/dev/themeinerlp/faweschematiccloud/commands/DownloadCommand.kt
@@ -0,0 +1,40 @@
+package dev.themeinerlp.faweschematiccloud.commands
+
+import cloud.commandframework.annotations.CommandMethod
+import com.fastasyncworldedit.core.configuration.Caption
+import com.fastasyncworldedit.core.extent.clipboard.MultiClipboardHolder
+import com.sk89q.worldedit.WorldEdit
+import com.sk89q.worldedit.bukkit.BukkitAdapter
+import com.sk89q.worldedit.util.formatting.text.event.ClickEvent
+import dev.themeinerlp.faweschematiccloud.FAWESchematicCloud
+import org.bukkit.entity.Player
+
+class DownloadCommand(
+    val faweSchematicCloud: FAWESchematicCloud
+) {
+
+    @CommandMethod("/download")
+    fun download(player: Player) {
+        val actor = BukkitAdapter.adapt(player)
+        val sessionManager = WorldEdit.getInstance().sessionManager
+        val session = sessionManager[actor]
+        val clipboard = session.clipboard
+        if (clipboard !is MultiClipboardHolder) {
+            actor.print(Caption.of("fawe.web.generating.link", "fast"))
+            faweSchematicCloud.schematicUploader.upload(clipboard).whenComplete { result, throwable ->
+                if (throwable != null || !result.success) {
+                    actor.print(Caption.of("fawe.web.generating.link.failed"))
+                } else {
+                    val download = result.apiDownloadUrl!!
+                    val frontEndDownload = result.apiDownloadUrl
+                    actor.print(
+                        Caption.of("fawe.web.download.link", frontEndDownload).clickEvent(ClickEvent.openUrl(download))
+                    )
+                }
+            }
+        }
+
+
+    }
+
+}
