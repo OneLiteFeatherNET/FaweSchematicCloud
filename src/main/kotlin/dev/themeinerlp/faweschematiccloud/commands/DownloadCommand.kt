a73f3fb737c40f0276a634ade1b5b6ac466f3ff9
TheMeinerLP <p.glanz@madfix.me>
2023-06-09 11:43:02 +0200

    Improve download command

--- src/main/kotlin/dev/themeinerlp/faweschematiccloud/commands/DownloadCommand.kt
+++ src/main/kotlin/dev/themeinerlp/faweschematiccloud/commands/DownloadCommand.kt
@@ -1,29 +1,34 @@
 package dev.themeinerlp.faweschematiccloud.commands
 
+import cloud.commandframework.annotations.Argument
 import cloud.commandframework.annotations.CommandMethod
 import cloud.commandframework.annotations.CommandPermission
+import cloud.commandframework.annotations.specifier.Greedy
 import com.fastasyncworldedit.core.configuration.Caption
 import com.fastasyncworldedit.core.extent.clipboard.MultiClipboardHolder
 import com.sk89q.worldedit.WorldEdit
 import com.sk89q.worldedit.bukkit.BukkitAdapter
+import com.sk89q.worldedit.extent.clipboard.io.BuiltInClipboardFormat
 import com.sk89q.worldedit.util.formatting.text.event.ClickEvent
 import dev.themeinerlp.faweschematiccloud.FAWESchematicCloud
+import dev.themeinerlp.faweschematiccloud.util.SchematicHolder
 import org.bukkit.entity.Player
 
 class DownloadCommand(
-    val faweSchematicCloud: FAWESchematicCloud
+        private val faweSchematicCloud: FAWESchematicCloud
 ) {
 
-    @CommandMethod("/download")
+    @CommandMethod("/download [format]")
     @CommandPermission("worldedit.clipboard.download")
-    fun download(player: Player) {
+    fun download(player: Player, @Argument("format", defaultValue = "fast") @Greedy format: BuiltInClipboardFormat) {
         val actor = BukkitAdapter.adapt(player)
         val sessionManager = WorldEdit.getInstance().sessionManager
         val session = sessionManager[actor]
         val clipboard = session.clipboard
         if (clipboard !is MultiClipboardHolder) {
-            actor.print(Caption.of("fawe.web.generating.link", "fast"))
-            faweSchematicCloud.schematicUploader.upload(clipboard).whenComplete { result, throwable ->
+            actor.print(Caption.of("fawe.web.generating.link", format))
+            val schematicHolder = SchematicHolder(clipboard, format)
+            faweSchematicCloud.schematicUploader.upload(schematicHolder).whenComplete { result, throwable ->
                 if (throwable != null || !result.success) {
                     actor.print(Caption.of("fawe.web.generating.link.failed"))
                 } else {
